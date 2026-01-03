1e8dfb7dba3f0e311edd7a763fcf4322ff048320
Tommy <tommy.nguyenn53@gmail.com>
2026-01-04 10:54:06 +1100

    Adding layout.tsx file in gift directory

---
+++ app/[slug]/gift/layout.tsx
@@ -0,0 +1,7 @@
+export default function GiftLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return <>{children}</>
+}
